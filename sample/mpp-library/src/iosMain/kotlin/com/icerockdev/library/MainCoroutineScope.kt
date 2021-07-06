d687ad1c9e849efe0f75686ee39361a593f099a0
Anton Shestak <ashestak@icerockdev.com>
2021-07-06 11:59:30 +0700

    #4 detekt fixes

--- sample/mpp-library/src/iosMain/kotlin/com/icerockdev/library/MainCoroutineScope.kt
+++ sample/mpp-library/src/iosMain/kotlin/com/icerockdev/library/MainCoroutineScope.kt
@@ -5,9 +5,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 
-class MainCoroutineScope(): CoroutineScope by CoroutineScope(Dispatchers.Main + SupervisorJob())
-{
-   fun close(){
-       this.cancel()
-   }
+class MainCoroutineScope : CoroutineScope by CoroutineScope(Dispatchers.Main + SupervisorJob()) {
+    fun close() {
+        this.cancel()
+    }
 }
